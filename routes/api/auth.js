ce16a7227bc6101d7002a863c88e0d101f0d763d
CheCoelho <checoelho@gmail.com>
2021-02-07 23:25:45 +0200

    Nearly finished backend

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const auth = require('../../middleware/auth')
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');           //removed '/check' based on deprication warning, which was not indicated in the tutorial.
 
 const User = require('../../models/User')
 
@@ -10,7 +14,7 @@ const User = require('../../models/User')
 router.get('/', auth, async (req, res) => {
     try {
         
-        const user = await User.findById(req.user._id).select('-password');                  //.select excludes password
+        const user = await User.findById(req.user.id).select('-password');                  //.select excludes password
         
         res.json(user)
     } catch(err) {
@@ -19,6 +23,65 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error')
     }
 });
+
+
+/// @route      POST api/auth
+//  @desc       Authenticate User & Get Token
+//  @access     Public
+router.post('/', [
+     
+    check('email', 'Please include a valid email').isEmail(),                       //check that email is valid
+    check('password', 'Password is required')
+    .exists()                                                                       //check password exists
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body
+
+        try{
+            let user = await User.findOne({ email });
+
+            if(!user) {
+                return res.status(400)
+                .json({ errors: [ { ms: 'Invalid Credentials' } ] });                  //check if user exists to avoid to users with same email address
+            }
+            
+
+            //Make sure password matches found user
+            const isMatch = await bcrypt.compare(password, user.password);              //first param is users plain text entered password, second is the encrypted one in the DB
+
+            if(!isMatch) {
+                return res.status(400)
+                .json({ errors: [ { ms: 'Invalid Credentials' } ] });                  //check if user s entered password matches stored encrypted password
+            }
+            
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, 
+                config.get('jwtSecret'),
+                    { expiresIn: 360000 },
+                    (err, token) => {
+                    if (err) throw err;
+                    res.json({ token })
+                    }
+                );
+
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+
+    });
+
 
 
 module.exports = router;
